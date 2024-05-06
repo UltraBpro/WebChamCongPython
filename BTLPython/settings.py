2dc072dde84d2fdf7147b49930d59c3125069d96
TrueSuperiorBpro <b3bpro123@gmail.com>
2024-05-06 10:17:49 +0700

    Test init

--- BTLPython/settings.py
+++ BTLPython/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9cvds4rztat8!=d+1b!t-30txp%eyyb*mk@zfhfhd8e5=##!@9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.10.29.241','127.0.0.1']
+ALLOWED_HOSTS = ['10.10.29.241','127.0.0.1','10.10.29.187','192.168.11.5']
 
 
 # Application definition
